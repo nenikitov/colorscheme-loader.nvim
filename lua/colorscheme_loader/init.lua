03aed76f465291f35fc61ef9b2a302e37466690d
nenikitov <nenikitov@mail.ru>
2025-04-24 14:27:09 -0400

    refactor: entire thing

--- lua/colorscheme_loader/init.lua
+++ lua/colorscheme_loader/init.lua
@@ -30,16 +30,17 @@ local M = {}
 ---@class Config
 ---@field custom string | string[] | nil Custom colorschemes in order that they should be tried to be applied
 ---@field fallback DefaultColorscheme Default colorscheme to fall back on
+---@field silent boolean | nil Whether to ignore all logs
 
 ---@type Config
 M.default_config = {
     fallback = 'default',
+    silent = false
 }
 
 ---@param colorscheme string
-function M.try_apply_colorscheme(colorscheme)
-    ---@diagnostic disable-next-line: param-type-mismatch -- `vim.cmd` is indeed callable
-    local status, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
+function M.apply_colorscheme(colorscheme)
+    local status, _ = pcall(vim.cmd.colorscheme, colorscheme)
     return status
 end
 
@@ -48,47 +49,37 @@ end
 function M.setup(opts)
     local config = vim.tbl_deep_extend('force', M.default_config, opts)
 
-    -- Make `config.custom` only `string[]`
-    if config.custom == nil then
-        config.custom = {}
-    elseif type(config.custom) == 'string' then
-        ---@diagnostic disable-next-line: assign-type-mismatch -- `config.custom` is `string`
-        config.custom = { config.custom }
-    end
+    -- Make config.custom always be a string[]
+    ---@type string[]
+    ---@diagnostic disable-next-line: assign-type-mismatch -- The expression type is `string[]`
+    local colorschemes = (type(config.custom) == 'string' and { config.custom } or config.custom) or {}
+    table.insert(colorschemes, config.fallback)
 
-    -- Try to apply custom
-    if #config.custom ~= 0 then
-        ---@type string[]
-        local failed = {}
-        ---@type string | nil
-        local succeeded = nil
 
-        ---@diagnostic disable-next-line: param-type-mismatch -- `config.custom` is `string[]`
-        for _, c in ipairs(config.custom) do
-            if M.try_apply_colorscheme(c) then
-                succeeded = c
-                break
-            else
-                table.insert(failed, c)
-            end
-        end
+    -- Apply custom colorschemes
+    ---@type string[]
+    local failed = {}
+    ---@type string | nil
+    local succeeded = nil
 
-        if #failed ~= 0 then
-            vim.notify(
-                'Colorschemes '
-                    .. vim.inspect(failed)
-                    .. ' could not be loaded, falling back to '
-                    .. vim.inspect(succeeded or config.fallback),
-                vim.log.levels.WARN
-            )
-        end
-        if succeeded then
-            return
+    for _, c in ipairs(colorschemes) do
+        if M.apply_colorscheme(c) then
+            succeeded = c
+            break
+        else
+            table.insert(failed, c)
         end
     end
 
-    -- Apply default
-    M.try_apply_colorscheme(config.fallback)
+    if #failed ~= 0 and not config.silent then
+        vim.notify(
+            'Colorschemes '
+                .. vim.inspect(failed)
+                .. ' could not be loaded, falling back to '
+                .. vim.inspect(succeeded),
+            vim.log.levels.WARN
+        )
+    end
 end
 
 return M
